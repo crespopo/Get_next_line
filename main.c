2861ac344b843f960c23ae0d648d05647da86bdf
Daniel Crespo Gonzalez <dacrespo@c1r17s2.42madrid.com>
2025-02-22 18:57:25 +0100

    Norminette pasada y //stop

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dacrespo <dacrespo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:30:26 by dacrespo          #+#    #+#             */
-/*   Updated: 2025/02/22 18:07:22 by dacrespo         ###   ########.fr       */
+/*   Updated: 2025/02/22 18:55:42 by dacrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(void)
 	int		fd;
 	char	*line;
 	//int		stop;
+
 	fd = open("./miFichero.txt", O_RDONLY);
 	if (fd == -1)
 	{
@@ -24,12 +25,15 @@ int	main(void)
 		return (1);
 	}
 	line = get_next_line(fd);
-	////while (stop--)
+	//stop = 10;
+	//while (line && stop > 0)
 	while (line)
 	{
+
 		printf("\nEsto es la cadena guardada --->> %s\n", line);
 		free(line);
 		line = get_next_line(fd);
+		//stop--;
 	}
 	close (fd);
 	return (0);
